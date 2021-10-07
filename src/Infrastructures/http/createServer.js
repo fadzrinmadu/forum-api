81fe033a1e7009db3b3c243d78508ef3d367f880
fadzrinmadu <muhfadzrinmadu@gmail.com>
2021-10-08 00:50:29 +0700

    add say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -62,14 +62,6 @@ const createServer = async (container) => {
     },
   ]);
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => ({
-      value: 'Halo Dunia!',
-    }),
-  });
-
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
